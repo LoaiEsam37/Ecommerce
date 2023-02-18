7b7f603765563d7429c2193d275b434a0c73f57e
LoaiEsam37 <loaiesam1234@gmail.com>
2023-02-18 19:00:36 +0200

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Navbar from "./layouts/Navbar";
+import AboutUs from "./pages/AboutUs";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./sass/app.scss";
 
 function App() {
+  const client = new QueryClient();
   return (
-    <div className="App">
+    <QueryClientProvider client={client}>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
-      <div className="row">
-        <Home />
-      </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import Dropdown from "react-bootstrap/Dropdown";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import Collapse from "react-bootstrap/Collapse";
 
 const Navbar = () => {
@@ -7,9 +7,9 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <NavLink className="navbar-brand" to="/">
           Ecommerce
-        </a>
+        </NavLink>
         <button
           onClick={() => setOpen(!open)}
           className="navbar-toggler"
@@ -26,28 +26,24 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/"
+                  activeClassName="active"
+                >
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/AboutUs"
+                  activeClassName="active"
+                >
                   AboutUs
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <Dropdown>
-                  <Dropdown.Toggle variant="" id="dropdown-basic">
-                    Account
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#">myAccount</Dropdown.Item>
-                    <hr className="dropdown-divider" />
-                    <Dropdown.Item href="#">Orders</Dropdown.Item>
-                    <Dropdown.Item href="#">cart</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
+                </NavLink>
               </li>
               <hr />
             </ul>
